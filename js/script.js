ede436dc3fb2286e77c58815885cf99588eb46d2
imedsalhi <imedsalhi3010@gmail.com>
2020-01-28 08:09:38 +0000

    ajout qlq fonctions

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ $("#b_navigation").on("click", function() {
     $("#nav_bar").toggle();
 
 });
+var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+});
+var height = $(window).height();
+// if (height > 3000) {
+//     alert("Bla");
+//     // $('#bloc2').animate({"left": "+=300px"}, "slow");
+// }
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 830) {
+        $("#navbar").addClass("scrollabout");
+        $("li a").addClass("colortext");
+        // alert($(window).scrollTop());
+    } else {
+        $("#navbar").removeClass("scrollabout");
+        $("li a").removeClass("colortext");
+    }
+});
